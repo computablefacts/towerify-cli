f705421f4d4b4e6e639b8df12648c56143fbc1d0
Patrick Brisacier <pbrisacier@mncc.fr>
2023-11-04 03:46:59 +0100

    Refactor

--- towerify/src/lib/filters.sh
+++ towerify/src/lib/filters.sh
@@ -1,7 +1,17 @@
 filter_app_config_should_not_exist() {
-  [[ -f $app_config_file ]] && echo "$(error_app_config_file_already_exists)"
+  if [[ -f $app_config_file ]]; then
+    echo "$(red_bold "Le fichier $app_config_file existe déjà dans ce répertoire.")"
+    echo
+    echo "Si vous voulez effacer cette configuration pour en créer une nouvelle, utilisez :"
+    echo "  $(bold "towerify init --force")"
+  fi
 }
 
 filter_app_config_should_exist() {
-  [[ -f $app_config_file ]] || echo "$(error_app_config_file_does_not_exist)"
+  if [[ ! -f $app_config_file ]]; then
+    echo "$(red_bold "Le fichier $app_config_file n'existe pas dans ce répertoire.")"
+    echo
+    echo "Si vous voulez initialiser cette application, utilisez :"
+    echo "  $(bold "towerify init")"
+  fi
 }
