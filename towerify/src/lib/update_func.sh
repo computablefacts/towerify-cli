8a5712723cd1b43446945c62fee9f8c0e025637f
Patrick Brisacier <pbrisacier@mncc.fr>
2024-07-11 21:38:54 +0200

    Compare version without dpkg (needed for MacOS support)

--- towerify/src/lib/update_func.sh
+++ towerify/src/lib/update_func.sh
@@ -17,9 +17,34 @@ download_towerify() {
 }
 
 available_version_is_newer() {
-  new_version=$1
+  local new_version="$1"
 
-  dpkg --compare-versions $new_version gt $version
+  # Découper les versions en parties avec le point comme séparateur
+  IFS='.' read -ra new_version_parts <<< "$new_version"
+  IFS='.' read -ra version_parts <<< "$version"
+
+  # Déterminer le plus grand nombre de parties
+  local length_max=${#new_version_parts[@]}
+  if [ ${#version_parts[@]} -gt $length_max ]; then
+      length_max=${#version_parts[@]}
+  fi
+
+  for ((i = 0; i < length_max; i++)); do
+      # Traiter les parties manquantes comme des zéros
+      local new_version_part=${new_version_parts[i]:-0}
+      local version_part=${version_parts[i]:-0}
+
+      if [ "$new_version_part" -lt "$version_part" ]; then
+          # new_version est inférieure à version
+          return 1
+      elif [ "$new_version_part" -gt "$version_part" ]; then
+          # new_version est supérieure à version
+          return 0
+      fi
+  done
+
+  # Les versions sont égales
+  return 1
 }
 
 towerify_update() {
